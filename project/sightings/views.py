bafac980bf7b8ba3359fd2ac201f921527c962d4
Zijie Chen <zc2416@columbia.edu>
2019-12-06 04:34:49 +0000

    lock the project

--- project/sightings/views.py
+++ project/sightings/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from .models import Sighting
-from .forms import Pet
+from .forms import SightingForm
 
 def index(request):
     return HttpResponse('Hi')
