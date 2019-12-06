5407154b2ba93e53680dc992373cd8667b628536
Zijie Chen <zc2416@columbia.edu>
2019-12-06 05:32:52 +0000

    update views add list v2

--- project/sightings/views.py
+++ project/sightings/views.py
@@ -4,6 +4,26 @@ from django.http import HttpResponse
 from .models import Sighting
 from .forms import SightingForm
 
-def index(request):
-    return HttpResponse('Hi')
+def all_sightings(request):
+    sightings = Sighting.objects.all()
+    context = {
+            'sightings': sightings,
+    }
+    return render(request,'sightings/all.html', context)
+
+
+def add_sighting(request):
+    if request.method == 'POST'
+        form = PetForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(f'/sightings/list/')
+    else:
+        form = PetForm()
+
+    context = {
+            'form': form,
+    }
+
+    return render(request, 'sightings/edit.html', context)
 # Create your views here.
