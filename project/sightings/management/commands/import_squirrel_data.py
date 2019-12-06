19c69a0ec19cd993344dcc66df05551b151d48f2
Zijie Chen <zc2416@columbia.edu>
2019-12-06 07:53:27 +0000

    deleted these two files by error and repush them

--- project/sightings/management/commands/import_squirrel_data.py
+++ project/sightings/management/commands/import_squirrel_data.py
@@ -1,155 +1,91 @@
-from django.db import models
+from django.core.management.base import BaseCommand, CommandError
+from sightings.models import Sighting #as "something" needed?
+import os
+import csv
+from django.conf import settings
+#from data_import.settings import BASE_DIR
+import datetime as dt
 
-from django.utils.translation import gettext as _
+class Command(BaseCommand):
+    help = 'Import csv data into database'
 
-class Sighting(models.Model):
+    def add_arguments(self, parser):
+        parser.add_argument('file_path', help ='file path of csv data')
 
-    id = models.CharField(
-        help_text=_('Squirrel ID'),
-        max_length=255,
-        primary_key=True,
-    )
-    
-    ADULT = 'Adult'
-    JUVENILE = 'Juvenile'
 
-    AGE_CHOICES = (
-        (ADULT, 'Adult'),
-        (JUVENILE, 'Juvenile'),
-    )
+    def import_data_from_csv(self, filename):
+        with open(filename, 'r') as csv_file:
+            data = csv.reader(csv_file, delimiter=',')
+            headers = next(data)
+            for data_object in data:
+                id =  data_object[2]
+                age = data_object[7]
+                color = data_object[8]
+                latitude = data_object[1]
+                longitude = data_object[0]
+                date = dt.datetime.strptime(data_object[5].strip(),'%m%d%Y').date()
+                location = data_object[12]
+                specific_location = data_object[14]
+                pre_running = data_object[15]
+                running = True if "true" in pre_running.lower() else False
+                pre_chasing = data_object[16]
+                chasing = True if "true" in pre_chasing.lower() else False
+                pre_climbing = data_object[17]
+                climbing  = True if "true" in pre_climbing.lower() else False
+                pre_eating = data_object[18]
+                eating = True if "true" in pre_eating.lower() else False
+                pre_foraging = data_object[19]
+                foraging = True if "true" in pre_foraging.lower() else False
+                other_activities = data_object[20]
+                pre_kuks = data_object[21]
+                kuks = True if "true" in pre_kuks.lower() else False
+                pre_quaas = data_object[22]
+                quaas = True if "true" in pre_quaas.lower() else False
+                pre_moans = data_object[23]
+                moans  = True if "true" in pre_moans.lower() else False
+                pre_tail_flags = data_object[24]
+                tail_flags = True if "true" in pre_tail_flags.lower() else False
+                pre_tail_twitches = data_object[25]
+                tail_twitches  = True if "true" in pre_tail_twitches.lower() else False
+                pre_approaches = data_object[26]
+                approaches  = True if "true" in pre_approaches.lower() else False
+                pre_indifferent = data_object[27]
+                indifferent = True if "true" in pre_indifferent.lower() else False
+                pre_runs_from = data_object[28]
+                runs_from = True if "true" in pre_runs_from.lower() else False
+                try:
+                    sighting, created = Sighting.objects.get_or_create(
+                        id=id,
+                        age=age,
+                        color=color,
+                        latitude=latitude,
+                        longitude=longitude,
+                        date=date,
+                        location=location,
+                        specific_location=specific_location,
+                        running=running,
+                        chasing=chasing,
+                        climbing=climbing,
+                        eating=eating,
+                        foraging=foraging,
+                        other_activities=other_activities,
+                        kuks=kuks,
+                        quaas=quaas,
+                        moans=moans,
+                        tail_flags=tail_flags,
+                        tail_twitches=tail_twitches,
+                        approaches=approaches,
+                        indifferent=indifferent,
+                        runs_from=runs_from
+                    )
+                    if created:
+                        sighting.save()
+                        display_format = "\nSighting, {}, has been saved."
+                        print(display_format.format(sighting))
+                except Exception as ex:
+                    print(str(ex))
 
-    age = models.CharField(
-        help_text=_('Squirel Age'),
-        max_length=20,
-        choices=AGE_CHOICES,
-        blank = True,
-    )
+    def handle(self, *args, **options):
+        file_path = options['file_path']
+        self.import_data_from_csv(file_path)
 
-    GRAY = 'Gray'
-    CINNAMON = 'Cinnamon'
-    BLACK = 'Black'
-
-    COLOR_CHOICES = (
-        (GRAY, 'Gray'),
-        (CINNAMON, 'Cinnamon'),
-        (BLACK, 'Black'),
-    )
-
-    color = models.CharField(
-        help_text=_('Squirrel Fur Color'),
-        max_length=100,
-        choices=COLOR_CHOICES,
-        blank = True,
-    )
-
-    latitude = models.DecimalField(
-        help_text=_('Latitude'),
-        max_digits=15,
-        decimal_places=15, 
-    )
-
-    longitude = models.DecimalField(
-        help_text=_('Longitude'),
-        max_digits=15,
-        decimal_places=15, 
-    )
-
-    date = models.DateField(
-        help_text=_('Sighting Date'),
-    )
-
-    GROUND_PLANE = 'Ground Plane'
-    ABOVE_GROUND = 'Above Ground'
-
-    LOCATION_CHOICES = (
-        (GROUND_PLANE, 'Ground Plane'),
-        (ABOVE_GROUND, 'Above Ground'),
-    )
-
-    location = models.CharField(
-        help_text=_('Location'),
-        max_length=100,
-        choices=LOCATION_CHOICES,
-        blank = True,
-        
-    )
-
-    specific_location = models.CharField(
-        help_text=_('Specific Location'),
-        max_length=255,
-        blank=True,
-    )
-
-    running = models.BooleanField(
-        help_text=_('Running'),
-        default=False,
-    )
-
-    chasing = models.BooleanField(
-        help_text=_('Chasing'),
-        default=False,
-    )
-
-    climbing = models.BooleanField(
-        help_text=_('Climbing'),
-        default=False,
-    )
-
-    eating = models.BooleanField(
-        help_text=_('Eating'),
-        default=False,
-    )
-
-    foraging = models.BooleanField(
-        help_text=_('Foraging'),
-        default=False,
-    )
-
-    other_activities = models.CharField(
-        help_text=_('Other Activities'),
-        max_length=255,
-        blank=True,
-    )
-
-    kuks = models.BooleanField(
-        help_text=_('Kuks'),
-        default=False,
-    )
-   
-    quaas = models.BooleanField(
-        help_text=_('Quaas'),
-        default=False,
-    )
-
-    moans = models.BooleanField(
-        help_text=_('Moans'),
-        default=False,
-    )
-
-    tail_flags = models.BooleanField(
-        help_text=_('Tail flags'),
-        default=False,
-    )
-   
-    tail_twitches = models.BooleanField(
-        help_text=_('Tail twitches'),
-        default=False,
-    )
-   
-    approaches = models.BooleanField(
-        help_text=_('Approaches'),
-        default=False,
-    )
-
-    indifferent = models.BooleanField(
-        help_text=_('Indifferent'),
-        default=False,
-    )
-   
-    runs_from = models.BooleanField(
-        help_text=_('Runs from'),
-        default=False,
-    )
-
-    def __str__(self):
-        return self.id
--- project/sightings/models.py
+++ project/sightings/models.py
@@ -1,3 +1,156 @@
 from django.db import models
 
-# Create your models here.
+from django.utils.translation import gettext as _
+
+class Sighting(models.Model):
+
+    id = models.CharField(
+        help_text=_('Squirrel ID'),
+        max_length=255,
+        primary_key=True,
+    )
+    
+    ADULT = 'Adult'
+    JUVENILE = 'Juvenile'
+
+    AGE_CHOICES = (
+        (ADULT, 'Adult'),
+        (JUVENILE, 'Juvenile'),
+    )
+
+    age = models.CharField(
+        help_text=_('Squirel Age'),
+        max_length=20,
+        choices=AGE_CHOICES,
+        blank = True,
+    )
+
+    GRAY = 'Gray'
+    CINNAMON = 'Cinnamon'
+    BLACK = 'Black'
+
+    COLOR_CHOICES = (
+        (GRAY, 'Gray'),
+        (CINNAMON, 'Cinnamon'),
+        (BLACK, 'Black'),
+    )
+
+    color = models.CharField(
+        help_text=_('Squirrel Fur Color'),
+        max_length=20,
+        choices=COLOR_CHOICES,
+        blank =True,
+
+    )
+
+    latitude = models.DecimalField(
+        help_text=_('Latitude'),
+        max_digits=19,
+        decimal_places=15,
+    )
+
+    longitude = models.DecimalField(
+        help_text=_('Longitude'),
+        max_digits=19,
+        decimal_places=15,
+    )
+
+    date = models.DateField(
+        help_text=_('Sighting Date'),
+    )
+
+    GROUND_PLANE = 'Ground Plane'
+    ABOVE_GROUND = 'Above Ground'
+
+    LOCATION_CHOICES = (
+        (GROUND_PLANE, 'Ground Plane'),
+        (ABOVE_GROUND, 'Above Ground'),
+    )
+
+    location = models.CharField(
+        help_text=_('Location'),
+        max_length=20,
+        choices=LOCATION_CHOICES,
+        blank = True,
+    )
+
+    specific_location = models.CharField(
+        help_text=_('Specific Location'),
+        max_length=255,
+        blank=True,
+    )
+
+    running = models.BooleanField(
+        help_text=_('Running'),
+        default=True,
+    )
+
+    chasing = models.BooleanField(
+        help_text=_('Chasing'),
+        default=True,
+    )
+
+    climbing = models.BooleanField(
+        help_text=_('Climbing'),
+        default=True,
+    )
+
+    eating = models.BooleanField(
+        help_text=_('Eating'),
+        default=True,
+    )
+
+    foraging = models.BooleanField(
+        help_text=_('Foraging'),
+        default=True,
+    )
+
+    other_activities = models.CharField(
+        help_text=_('Other Activities'),
+        max_length=255,
+        blank=True,
+    )
+
+    kuks = models.BooleanField(
+        help_text=_('Kuks'),
+        default=True,
+    )
+   
+    quaas = models.BooleanField(
+        help_text=_('Quaas'),
+        default=True,
+    )
+
+    moans = models.BooleanField(
+        help_text=_('Moans'),
+        default=True,
+    )
+
+    tail_flags = models.BooleanField(
+        help_text=_('Tail flags'),
+        default=True,
+    )
+   
+    tail_twitches = models.BooleanField(
+        help_text=_('Tail twitches'),
+        default=True,
+    )
+   
+    approaches = models.BooleanField(
+        help_text=_('Approaches'),
+        default=True,
+    )
+
+    indifferent = models.BooleanField(
+        help_text=_('Indifferent'),
+        default=True,
+    )
+   
+    runs_from = models.BooleanField(
+        help_text=_('Runs from'),
+        default=True,
+    )
+
+    def __str__(self):
+        return self.id
+
