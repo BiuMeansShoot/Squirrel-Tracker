4890b9e2c244d05ed366e8bb588e09ba367e278d
Zijie Chen <zc2416@columbia.edu>
2019-12-06 20:58:32 +0000

    addting buttoms

--- project/sightings/urls.py
+++ project/sightings/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
         path('', views.all_sightings),
         path('add/', views.add_sighting),
+        #path("stats/", views.Stats),
+        #path("<unique_squirrel_tracker_id>/", edit_sighting),
         ]
